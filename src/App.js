df604420fee1531f14692e7da1a33c14d63eae85
Luke Prescott <luke.prescott@presport.us>
2019-10-08 16:48:45 -0400

    Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './css/main.scss';
 import './css/normalize.scss';
 
